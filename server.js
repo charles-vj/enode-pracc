81fe8833baa1e3a896e029eeabab0fd38cfc09ad
charles-vj <charles2019@iiitkottayam.ac.in>
2021-08-03 17:12:50 +0530

    express and middleware

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const http= require('http');
 
-const server = http.createServer((req,res) => {
-    console.log(req)
+const express = require('express');
+
+const app = express();
+
+app.use((req,res,next) => {
+    console.log('In the middleware');
 });
+
+const server = http.createServer(app);
 
 server.listen(3000);
